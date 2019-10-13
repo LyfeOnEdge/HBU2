30ed9e4ae14558c3349fa2df9dd4b2401b9a26f7
Lyfe <noemail@gmail.com>
2019-10-13 05:02:12 -0700

    Update injector_categoryframe.py

--- customwidgets/injector_categoryframe.py
+++ customwidgets/injector_categoryframe.py
@@ -3,6 +3,8 @@ from .storeappsquare import storeAppSquare
 
 class injector_categoryFrame(categoryFrame):
     def __init__(self,parent,controller,framework, repos):
+        self.last_packages = []
+        self.local_packages_handler = controller.local_packages_handler
         categoryFrame.__init__(self, parent,controller,framework,repos)
         framework.add_on_refresh_callback(lambda: self.rebuild())
 
